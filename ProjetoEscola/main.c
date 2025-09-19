dd682ebbd4ae3c4fc039828208c948ac8664a763
José Amando Marques <joseamando5974@gmail.com>
2025-09-19 16:19:05 -0300

    Inicializando repositorio

---
+++ ProjetoEscola/main.c
@@ -0,0 +1,48 @@
+#include "functions.h"
+
+int main()
+{
+    int sair = false;
+    int listaAlunos[tam_Aluno];
+    bem_Vindos();
+
+    while (!sair)
+    {
+        int opcaoMain = escolhaMain();
+
+        switch (opcaoMain)
+        {
+            case 0:
+            {
+                sair = true;
+                break;
+            }
+            case 1:
+            {
+                int sairAluno = false;
+
+                sairAluno = moduloAluno();
+                
+                if (sairAluno)
+                    break;
+            }
+            case 2:
+            {
+                printf("Professores\n");
+                break;
+            }
+            case 3:
+            {
+                printf("Disciplinas\n");
+                break;
+            }
+            default:
+            {
+                printf("Opcao invalida\n");
+                break;
+            }
+        }
+    }
+    fim_Programa();
+    system("pause");
+}
